c7f4427128b21307619631428d25152682ca84a6
yashwanthrao425@gmail.com <yashwanthrao425@gmail.com>
2023-07-29 19:22:09 +0530

    api concepts

---
+++ RestAssuredLearning/src/test/java/RestAssured/PathandQueryParameters.java
@@ -0,0 +1,27 @@
+package RestAssured;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+import org.testng.annotations.Test;
+
+public class PathandQueryParameters {
+
+	@Test
+	void pathandQueryParams() {
+
+		given()
+		.pathParam("mypath", "users")
+		.queryParam("page", 2)
+		.queryParam("id", 5)
+
+				.when().get("https://reqres.in/api/{mypath}")
+
+				.then()
+
+				.statusCode(200).log().all();
+
+	}
+
+}
